63c090400c48066c93a2a67cb1bdd2a09319c93b
darthpiotr <vader.piotr@gmail.com>
2020-07-01 18:48:38 +0200

    Created Algorithm class to manage algorithms. Basic concept of title/artist recognition algorithm

---
+++ YoutifyLib/YoutifyLib/Algorithm/Algorithm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YoutifyLib.Algorithm
+{
+    public static class Algorithm
+    {
+        public static char  [] generalDiv = { '-', '–' };
+        public static string[] artistDiv  = { " feat", " ft", " vs", " x " };
+        public static string GetArtist(string title, string channel)
+        {
+            return "";
+        }
+    }
+}
